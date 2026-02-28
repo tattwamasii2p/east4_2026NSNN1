30e8934fd4bc8cdd4d0f992533bb7f4ace4a17f2
tattwamasii2p <hypnosis.i2p@gmail.com>
2026-02-28 18:18:41 +0800

    Heterarchy of related identities introduces

--- src/main/java/org/east4/helpful_assistant/psyche/Psyche.java
+++ src/main/java/org/east4/helpful_assistant/psyche/Psyche.java
@@ -10,7 +10,6 @@ public interface Psyche {
 	Will getWill();
 	List<Fantasy> getFantasies();
 	Brain getLocalSubBrain();
-	List<Psyche> getSubidentitiesForAvatar(SoftwareCreatureAvatar avatar);
+	List<Psyche> getRelatedIdentitiesForAvatar(SoftwareCreatureAvatar avatar);
 	List<SoftwareCreatureAvatar> getAvatars();
-	List<Psyche> getSuperidentitiesForAvatar(SoftwareCreatureAvatar avatar);
 }
